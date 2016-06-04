938d59485dc152abe507fe8fc230335744a9fc4e
Nikita <exotickg1@gmail.com>
2016-06-05 00:35:16 +0600

    Content updated.

--- game/dota_addons/trollandelves/scripts/vscripts/abilities/ability_plunder.lua
+++
@@ -1,7 +0,0 @@
-function plunder(keys)
-    local caster = keys.caster
-    local damage = caster:GetAverageTrueAttackDamage()
-    caster:ModifyGold(damage, true, 0)
-
-    PopupGoldGain(caster,math.floor(damage))
-end
